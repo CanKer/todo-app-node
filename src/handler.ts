dd3e364edfe0845d761c0d3c586b52db10d40134
Jorge Arturo César Martínez <cdialike@gmail.com>
2020-08-15 01:23:01 +0200

    changes for heroku

--- src/handler.ts
+++
@@ -1,5 +0,0 @@
-import aws from "aws-serverless-express";
-import app from './app'
-const server = aws.createServer(app)
-
-exports.lambda = (event:any, context:any) => { aws.proxy(server, event, context) }
