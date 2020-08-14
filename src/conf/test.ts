53fd9df9d605853ce703b5d838b1434ac8606b46
Jorge Arturo César Martínez <cdialike@gmail.com>
2020-08-15 01:08:28 +0200

    removing env files

--- src/conf/test.ts
+++ src/conf/test.ts
@@ -10,7 +10,7 @@ const db: DB =  {
     key:      ``,
     dbname:   ``,
     dbUrl:    function() {
-      return `mongodb://mdm-fullstack-test:aK%238u0m%23qKhe3p3Iz*Ao@nonstopmongo-test-shard-00-00-kqfka.mongodb.net:27017,nonstopmongo-test-shard-00-01-kqfka.mongodb.net:27017,nonstopmongo-test-shard-00-02-kqfka.mongodb.net:27017/masterdata-test?ssl=true&replicaSet=NonstopMongo-Test-shard-0&authSource=admin&retryWrites=true`
+      return ``
     }
 }
 
